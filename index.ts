7bd6c986a56b2b8fac92bf4099eb8c975e7f97ac
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2022-07-21 16:52:27 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -7,67 +7,47 @@ interface Product {
   image: URL;
   limit: number;
   amount: number;
-
-  /*  function constructor(
-    name: string,
-    description: string,
-    price: number,
-    image: URL,
-    limit: number,
-    amount: number
-  ) {
-    this.name = name;
-    this.description = description;
-    this.price = price;
-    this.image = image;
-    this.limit = limit;
-    this.amount = amount;
-  } */
 }
 
 interface Cart {
   products: Product[];
-
-  /* constructor() {
-    this.products = [];
-  } */
 }
 
-function addProduct(cart: Cart, product: Product) {
+const addProduct = (cart: Cart, product: Product) => {
   cart.products.push(product);
-}
+};
 
-function removeProduct(cart: Cart, productToRemove: Product) {
+const removeProduct = (cart: Cart, productToRemove: Product) => {
   cart.products.forEach((product, i) => {
     if (product.name === productToRemove.name) cart.products.splice(i, 1);
   });
-}
+};
 
-function updateProductAmount(
+const updateProductAmount = (
   cart: Cart,
   productToChangeAmount: Product,
   amount: number
-) {
+) => {
   cart.products.forEach((product, i) => {
     if (product.name === productToChangeAmount.name) product.amount = amount;
   });
-}
+};
 
-function checkout(cart: Cart) {
+const checkout = (cart: Cart) => {
   cart.products = [];
-}
+};
 
-function totalPrice(cart: Cart): number {
+const totalPrice = (cart: Cart): number => {
   let sum: number = 0;
   cart.products.forEach((product, i) => {
     sum += product.amount * product.price;
   });
   return sum;
-}
+};
 
-function productQuantity(cart: Cart) {
+const productQuantity = (cart: Cart) => {
   return cart.products.length;
-}
+};
 
 let cart: Cart = { products: [] };
 
